41583d0ea99c9989dabec0b249c9e94c0d4e0dab
Keegan Obonyo <keeganobonyo@gmail.com>
2018-03-02 10:11:20 +0300

    initial commit

---
+++ authority/__init__.py
@@ -0,0 +1,23 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution('django-authority').version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+LOADING = False
+
+
+def autodiscover():
+    """
+    Goes and imports the permissions submodule of every app in INSTALLED_APPS
+    to make sure the permission set classes are registered correctly.
+    """
+    global LOADING
+    if LOADING:
+        return
+    LOADING = True
+
+    from authority import utils
+    utils.autodiscover_modules()
